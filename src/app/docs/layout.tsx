09e83d34158deef9a917ac61d7214e0b39efdf5f
akbarsaputrait <akbarsaputrait@outlook.com>
2025-12-22 21:24:13 +0700

    refactor: remove unused files and codes

--- src/app/docs/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { DocsLayout } from '@/components/docs/DocsLayout'
-
-export default function DocsLayoutPage({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <DocsLayout>{children}</DocsLayout>
-}
