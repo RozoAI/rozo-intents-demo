09126b98570d20bf6d43cc98a005507863229c2e
akbarsaputrait <akbarsaputrait@outlook.com>
2026-01-10 22:15:57 +0700

    feat: add leaderboard link

--- src/app/bridge/page.tsx
+++ src/app/bridge/page.tsx
@@ -1,18 +1,29 @@
+"use client";
+
 import { AnalyticsCard } from "@/components/new-bridge/AnalyticsCard";
 import { BridgeMain } from "@/components/new-bridge/BridgeMain";
 import { BridgeProvider } from "@/components/new-bridge/providers/BridgeProvider";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "ROZO Intents - Stellar",
-  description:
-    "Transfer USDC between Stellar and other chains with fast, secure, and gasless transactions",
-};
+import { useStellarWallet } from "@/contexts/StellarWalletContext";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 export default function NewPage() {
+  const { stellarAddress, stellarConnected } = useStellarWallet();
   return (
     <BridgeProvider>
       <div className="flex flex-col items-center gap-6 w-full">
+        {stellarConnected && (
+          <div className="w-full rounded-md bg-primary py-2 px-4 text-white text-center">
+            <Link
+              href={`https://app.rozo.ai/leaderboard?address=${stellarAddress}`}
+              className="flex items-center justify-center gap-2 text-sm font-medium"
+              target="_blank"
+            >
+              Wave 2 Leaderboard!
+              <ArrowRight className="size-4" />
+            </Link>
+          </div>
+        )}
         <BridgeMain />
         <div className="w-full max-w-xl">
           <AnalyticsCard />
